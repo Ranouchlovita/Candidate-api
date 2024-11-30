ab1f641fa3709ba0074df87c452aa939439dca0a
Rania Maamer <118470203+Ranouchlovita@users.noreply.github.com>
2024-11-30 16:24:40 +0100

    Update candidates.controller.ts

--- src/candidates/candidates.controller.ts
+++ src/candidates/candidates.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, NotFoundException } from '@nestjs/common';
 import { CandidatesService } from './candidates.service';
 import { CreateCandidateDto } from './candidate.dto';
 import { Candidate } from './candidates.entity';
@@ -18,12 +18,9 @@ export class CandidatesController {
     }
 
     @Get(':id')
-    async findOne(@Param('id') id: string): Promise<Candidate> {
-    const candidate = await this.candidatesService.findOne(+id);
-    if (!candidate) {
-        throw new NotFoundException(`Candidate with id ${id} not found`);
+    findOne(@Param('id') id: string): Promise<Candidate> {
+        return this.candidatesService.findOne(+id);
     }
-    return candidate;
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() updateCandidateDto: CreateCandidateDto): Promise<Candidate> {
@@ -35,3 +32,4 @@ export class CandidatesController {
         return this.candidatesService.remove(+id);
     }
 }
+
